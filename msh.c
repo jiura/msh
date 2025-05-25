dc32a7e11dfb814d8234b7a6e3f7ae94120ab539
Jiura <>
2025-05-25 03:42:11 -0300

    Loony refactor to make code more linear

--- msh.c
+++ msh.c
@@ -9,123 +9,8 @@
 #include <unistd.h>
 
 #define MSH_RL_BUFSIZE 1024
-
-/*char* old_msh_read_line(void) {
-	int bufsize = MSH_RL_BUFSIZE;
-	int position = 0;
-	char *buffer = malloc(sizeof(*buffer) * bufsize);
-	int c;
-
-	if (!buffer) {
-		fprintf(stderr, "msh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-
-	while (1) {
-		// Read a character
-		c = getchar();
-
-		// Replace with null terminator if it hits EOF or newline
-		if (c == EOF || c == '\n') {
-			buffer[position] = '\0';
-			return buffer;
-		} else {
-			buffer[position] = c;
-		}
-
-		++position;
-		
-		{ // Reallocate if buffer is exceeded
-			if (position >= bufsize) {
-				bufsize += MSH_RL_BUFSIZE;
-				buffer = realloc(buffer, bufsize);
-
-				if (!buffer) {
-					fprintf(stderr, "msh: allocation error\n");
-					exit(EXIT_FAILURE);
-				}
-			}
-		}
-	}
-}*/
-
-char* msh_read_line(void) {
-	char *line = NULL;
-	size_t bufsize = 0;
-
-	if (getline(&line, &bufsize, stdin) == -1) {
-		if (feof(stdin)) {
-			exit(EXIT_SUCCESS); // EOF received
-		} else {
-			perror("readline");
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	return line;
-}
-
 #define MSH_TOK_BUFSIZE 64
 #define MSH_TOK_DELIM " \t\r\n\a"
-
-char** msh_split_line(char *line) {
-	int bufsize = MSH_TOK_BUFSIZE, position = 0;
-
-	char *token;
-	char **tokens = malloc(bufsize * sizeof(*tokens));
-
-	if (!tokens) {
-		fprintf(stderr, "msh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-
-	while ((token = strtok_r(line, MSH_TOK_DELIM, &line))) {
-		tokens[position] = token;
-		++position;
-
-		if (position >= bufsize) {
-			bufsize += MSH_TOK_BUFSIZE;
-			tokens = realloc(tokens, bufsize * sizeof(*tokens));
-
-			if (!tokens) {
-				fprintf(stderr, "msh: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
-
-	tokens[position] = NULL;
-	return tokens;
-}
-
-int msh_launch(char **args) {
-	pid_t pid, wpid;
-	int status;
-
-	pid = fork();
-	if (pid == 0) {
-		// Child process
-
-		// NOTE: First item of args must be program name
-		if (execvp(args[0], args) == -1) {
-			perror("msh");
-		}
-
-		// NOTE: If execvp() returns, something went wrong
-		exit(EXIT_FAILURE);
-	} else if (pid < 0) {
-		// Error forking
-
-		perror("msh");
-	} else {
-		do {
-			// TODO: Could I let process run on background instead?
-			wpid = waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
-
-	return 1;
-}
 
 /*-- CODE FOR BUILT IN COMMANDS --*/
 
@@ -133,21 +18,11 @@ int msh_cd(char **args);
 int msh_help(char **args);
 int msh_exit(char **args);
 
-char *builtin_str[] = {
-	"cd",
-	"help",
-	"exit"
-};
+char *builtin_str[] = {"cd", "help", "exit"};
 
-int (*builtin_func[]) (char **) = {
-	&msh_cd,
-	&msh_help,
-	&msh_exit
-};
+int (*builtin_func[])(char **) = {&msh_cd, &msh_help, &msh_exit};
 
-int msh_num_builtins() {
-	return sizeof(builtin_str) / sizeof(*builtin_str);
-}
+int msh_num_builtins() { return sizeof(builtin_str) / sizeof(*builtin_str); }
 
 int msh_cd(char **args) {
 	if (args[1] == NULL) {
@@ -164,35 +39,26 @@ int msh_help(char **args) {
 	printf("Following cmds are built in:\n\n");
 
 	for (int i = 0; i < msh_num_builtins(); ++i) {
-		printf("  %s\n", builtin_str[i]);	
+		printf("  %s\n", builtin_str[i]);
 	}
 
 	printf("\nUse the man command for information on other programs.\n\n");
 	return 1;
 }
 
-int msh_exit(char **args) {
-	return 0;
-}
+int msh_exit(char **args) { return 0; }
 
 /*-- END --*/
 
-int msh_execute(char **args) {
-	if (args[0] == NULL) {
-		// Empty command
-		return 1;
-	}
+void msh_loop(void) {
 
-	for (int i = 0; i < msh_num_builtins(); ++i) {
-		if (strcmp(args[0], builtin_str[i]) == 0) {
-			return (*builtin_func[i])(args); 
-		}
-	}
-
-	return msh_launch(args);
 }
 
-void msh_loop(void) {
+int main(int argc, char **argv) {
+	/*-- LOAD CONFIG FILES START --*/
+	/*-- LOAD CONFIG FILES END --*/
+
+	/*-- COMMAND LOOP START --*/
 	char *line;
 	char **args;
 	int status;
@@ -200,22 +66,105 @@ void msh_loop(void) {
 	do {
 		printf("> ");
 
-		line = msh_read_line();
-		args = msh_split_line(line);
-		status = msh_execute(args);
+		{ /* Read input line */
+			line = NULL;
+			size_t bufsize = 0;
 
+			if (getline(&line, &bufsize, stdin) == -1) {
+				if (feof(stdin)) {
+					exit(EXIT_SUCCESS); // EOF received
+				} else {
+					perror("readline");
+					exit(EXIT_FAILURE);
+				}
+			}
+		}
+
+		{ /* Split line into args */
+			int bufsize = MSH_TOK_BUFSIZE, position = 0;
+
+			char *token;
+			args = malloc(bufsize * sizeof(*args));
+
+			if (!args) {
+				fprintf(stderr, "msh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+
+			// NOTE: strtok_r updates the address pointed by the third
+			// parameter to the next token's every time it is called.
+			// Use tmp to keep original address on line so it can be
+			// freed later
+			char *tmp_line = line;
+			while ((token = strtok_r(tmp_line, MSH_TOK_DELIM, &tmp_line))) {
+				args[position] = token;
+				++position;
+
+				if (position >= bufsize) {
+					bufsize += MSH_TOK_BUFSIZE;
+					args = realloc(args, bufsize * sizeof(*args));
+
+					if (!args) {
+						fprintf(stderr, "msh: allocation error\n");
+						exit(EXIT_FAILURE);
+					}
+				}
+			}
+
+			args[position] = NULL;
+		}
+
+		/* Execute command */
+
+		// Empty command
+		if (args[0] == NULL) {
+			status = 1;
+			goto status_found;
+		}
+
+		// Launch builtin
+		for (int i = 0; i < msh_num_builtins(); ++i) {
+			if (strcmp(args[0], builtin_str[i]) == 0) {
+				status = (*builtin_func[i])(args);
+				goto status_found;
+			}
+		}
+
+		// Launch process
+		pid_t pid, wpid;
+
+		pid = fork();
+		if (pid == 0) {
+			// Child process
+
+			// NOTE: First item of args must be program name
+			if (execvp(args[0], args) == -1) {
+				perror("msh");
+			}
+
+			// NOTE: If execvp() returns, something went wrong
+			exit(EXIT_FAILURE);
+		} else if (pid < 0) {
+			// Error forking
+
+			perror("msh");
+		} else {
+			do {
+				// TODO: Could I let process run on background instead?
+				wpid = waitpid(pid, &status, WUNTRACED);
+			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		}
+
+		status = 1;
+
+	status_found:
 		free(line);
 		free(args);
 	} while (status);
-}
+	/*-- COMMAND LOOP END --*/
 
-int main(int argc, char **argv) {
-	// Load config files
-	
-	// Command loop
-	msh_loop();
+	/*-- SHUTDOWN/CLEANUP START --*/
+	/*-- SHUTDOWN/CLEANUP END --*/
 
-	// Shutdown/cleanup
-	
 	return EXIT_SUCCESS;
 }
